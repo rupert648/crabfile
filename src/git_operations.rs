98ee809e4ee1b46c7ad96c9ab7fda766fc14d773
Rupert Carr <rupert@cloudflare.com>
2024-12-20 17:08:09 +0000

    added pre-commit hook to commit actual files instead of symlinks

--- src/git_operations.rs
+++ src/git_operations.rs
@@ -1,3 +1,7 @@
+use std::fs::{self, File};
+use std::io::Write;
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
 use std::process::Command;
 
 pub fn init_git_repo(repo_path: &str) -> Result<(), std::io::Error> {
@@ -5,5 +9,28 @@ pub fn init_git_repo(repo_path: &str) -> Result<(), std::io::Error> {
         .arg("init")
         .current_dir(repo_path)
         .output()?;
+    Ok(())
+}
+
+pub fn add_pre_commit_hook(repo_path: &str) -> Result<(), std::io::Error> {
+    let hook_contents = fs::read_to_string("sym-link-pre-commit.sh")?;
+
+    let hook_path = Path::new(repo_path)
+        .join(".git")
+        .join("hooks")
+        .join("pre-commit");
+
+    if let Some(parent) = hook_path.parent() {
+        fs::create_dir_all(parent)?;
+    }
+
+    let mut file = File::create(&hook_path)?;
+    file.write_all(hook_contents.as_bytes())?;
+
+    // Make the file executable (rwxr-xr-x = 0o755)
+    let mut perms = file.metadata()?.permissions();
+    perms.set_mode(0o755);
+    fs::set_permissions(&hook_path, perms)?;
+
     Ok(())
 }
